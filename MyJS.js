3d79d600867f313acc8594e0f343ff901b8b5683
Alexander Cardaras <acardara@uci.edu>
2018-09-26 16:18:16 -0700

    fix3

--- MyJS.js
+++ MyJS.js
@@ -6,3 +6,21 @@ function ReColorClick() {
         col = "blue";
     document.body.style.backgroundColor = col;
 }
+function readTextFile(file)
+{
+    document.body.style.backgroundColor = "red";
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
